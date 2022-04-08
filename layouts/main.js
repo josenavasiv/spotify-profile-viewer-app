009ac6183fc628bfb3ac485023bf0db161c7bfb6
Alexander <josenavasiv@gmail.com>
2022-04-07 17:13:46 -0700

    Added a simple page transition and loading spinner WIP

---
+++ layouts/main.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+
+const main = ({children, router}) => {
+  return (
+	<div>main</div>
+  )
+}
+
+export default main
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,32 @@
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
+import { AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+const variants = {
+	hidden: { opacity: 0, x: 0, y: 20 },
+	enter: { opacity: 1, x: 0, y: 0 },
+	exit: { opacity: 0, x: 0, y: -20 },
+};
+
+function MyApp({ Component, pageProps: { session, ...pageProps }, router }) {
 	return (
-		<SessionProvider session={session}>
-			<Component {...pageProps} />
-		</SessionProvider>
+		<AnimatePresence exitBeforeEnter initial={true}>
+			<motion.div
+				initial="hidden"
+				animate="enter"
+				exit="exit"
+				variants={variants}
+				transition={{ duration: 0.4, type: 'easeInOut' }}
+				style={{ position: 'relative' }}
+				key={router.pathname}
+			>
+				<SessionProvider session={session}>
+					<Component {...pageProps} key={router.route} />
+				</SessionProvider>
+			</motion.div>
+			;
+		</AnimatePresence>
 	);
 }
 
