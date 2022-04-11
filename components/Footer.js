c9c252fce3b8f1f303e9fcb0daa5697f1e46da05
Alexander <josenavasiv@gmail.com>
2022-04-10 20:48:47 -0700

    Added nextjs HEAD element

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,13 @@
 import Image from 'next/image';
+import Head from 'next/head';
 
 const Footer = () => {
 	return (
 		<div className="flex flex-col justify-center items-center w-full bottom-0 h-12 mb-6">
+			<Head>
+				<title>Spofity Profile Viewer</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
 			<a href="https://github.com/josenavasiv/spotify-profile-viewer-app" target="_blank">
 				<Image src="/github.svg" width="40" height="40" />
 			</a>
